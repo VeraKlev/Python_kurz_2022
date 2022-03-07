a5d49a5b4634320639c1ad94bb95f97cf9b07c0b
VeraKlev <vera.klevarova@gmail.com>
2022-03-07 17:58:14 +0100

    ukol_baliky

---
+++ baliky_2.py
@@ -0,0 +1,17 @@
+baliky = {
+    "B541X": True,
+    "B547X": False,
+    "B251X": False,
+    "B501X": True,
+    "B947X": False,
+}
+
+kod_baliku = input('Jaký je váš kód balíku? ')
+
+if kod_baliku ==  "B541X" or "B501X":
+    print(f"Balík byl předán kurýrovi.")
+
+else:
+    print(f"Balík zatím nebyl předán kurýrovi.")
+
+#zkoušela jsem různá řešení, ale buď mi vybíhají oba výstupy, nebo stále dokola jeden. 
