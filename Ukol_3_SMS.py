03b6bafdea7d80c9c7aaf0f78e78c66a590d951c
VeraKlev <vera.klevarova@gmail.com>
2022-03-27 22:54:10 +0200

    Ukol 3 SMS

--- Ukol_3_SMS.py
+++ Ukol_3_SMS.py
@@ -1,35 +1,31 @@
+import math
 telefonni_cislo = input("Jaké je vaše telefonní číslo? ")
-
-def delka_cisla(cislo):
-    pocet_znaku = len(telefonni_cislo)
+ 
+def delka_cisla(t_cislo):
+    t_cislo = t_cislo.replace(" ", "")
+    pocet_znaku = len(t_cislo)
     if pocet_znaku == 9:
         return True
     elif pocet_znaku == 13:
-        if "+420"in telefonni_cislo:
+        if "+420"in t_cislo:
             return True
         else:
             return False
     else:
         return False
-
-delka_cisla(telefonni_cislo)
-
+ 
+# delka_cisla(telefonni_cislo)
+ 
 if delka_cisla(telefonni_cislo) == False:
     print("Toto číslo není správně.")
 else:
     text_zpravy = input("Napište text vaší SMS zprávy: ")
-
-def cena_sms(pocet_znaku):
-    pocet_znaku = len(text_zpravy)
-    if pocet_znaku <= 180:
-        vysledek = 3
-    else:
-        vypocet = pocet_znaku / 180
-        import math
-        vysledek = math.ceil(vypocet) * 3
+ 
+def cena_sms(text_sms):
+    pocet_znaku_sms = len(text_sms)
+    vypocet = pocet_znaku_sms / 180
+    vysledek = math.ceil(vypocet) * 3
     print(f"Cena vaší SMS je {vysledek} Kč.")
-
+ 
 if delka_cisla(telefonni_cislo) == True:
     cena_sms(text_zpravy)
-
-    
